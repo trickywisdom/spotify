a7889324ab91f65d541d80b598c82d2986cb3c56
Spyros Trimis <spyrostrimis@gmail.com>
2023-04-21 01:07:06 +0300

    to search

---
+++ client/src/useAuth.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function useAuth(code) {
+    const [accessToken, setAccessToken] = useState()
+    const [refreshToken, setRefreshToken] = useState()
+    const [expiresIn, setExpiresIn] = useState()
+
+    useEffect(() => {
+      console.log("I fire once");
+      axios
+        .post("http://localhost:8080/login", {
+          code,
+        })
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          setRefreshToken(res.data.refreshToken);
+          setExpiresIn(res.data.expiresIn);
+          window.history.pushState({}, null, "/");
+        })
+        .catch(() => {
+          window.location = "/";
+        });
+    }, [code]);
+  
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+    const interval = setInterval(() => {
+      axios
+        .post("http://localhost:8080/refresh", {
+          refreshToken,
+        })
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          setExpiresIn(res.data.expiresIn);
+        })
+        .catch(() => {
+          window.location = "/";
+        });
+    }, (expiresIn - 60) * 1000);
+
+    return () => clearInterval(interval);
+  }, [refreshToken, expiresIn]);
+  
+  return accessToken
+}
+
+export default useAuth;
